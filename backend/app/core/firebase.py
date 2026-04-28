30b1ac03ca0d7c804b9803fd7d6b8dc072d30287
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-04-28 12:49:39 -0300

    pronto para o render

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -1,8 +1,25 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("app/core/firebase.json")
 
-firebase_admin.initialize_app(cred)
+def get_firebase_cred():
+    firebase_env = os.getenv("FIREBASE_CREDENTIALS")
+
+    # PRODUÇÃO (Render)
+    if firebase_env:
+        cred_dict = json.loads(firebase_env)
+        return credentials.Certificate(cred_dict)
+
+    # LOCAL
+    return credentials.Certificate(
+        "app/core/firebase.json"
+    )
+
+
+if not firebase_admin._apps:
+    cred = get_firebase_cred()
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,18 +1,40 @@
+import os
 from datetime import datetime, timedelta
 from jose import jwt
 
-SECRET_KEY = "super-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "super-secret-key"
+)
+
+ALGORITHM = os.getenv(
+    "ALGORITHM",
+    "HS256"
+)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv(
+        "ACCESS_TOKEN_EXPIRE_MINUTES",
+        60
+    )
+)
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
 
-    to_encode.update({"exp": expire})
+    to_encode.update({
+        "exp": expire
+    })
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
 
     return encoded_jwt
